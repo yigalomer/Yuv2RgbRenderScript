dc14af136b4987650c717d0af6b1908b789777f2
Yigal Omer <yigal.omer@gmail.com>
2014-02-03 00:21:20 +0200

    Separate render script functionality from main activity

--- src/com/example/yuv2rgbrenderscript/MainActivity.java
+++ src/com/example/yuv2rgbrenderscript/MainActivity.java
@@ -39,13 +39,9 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Ca
 
 	private Camera mCamera;
 	private RenderScript mRs ;
-
 	private SurfaceHolder mHolder;
-
 	private int mBufferSize ;
-
 	private SurfaceView mPreview;
-
 	private int mFrameCount;
 
 
@@ -151,9 +147,10 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Ca
 
 
 
-	// Method is called for each frame in the camera preview.
-	// We'll convert the frame data (byte[] data) from YUV to RGB and apply 
+	// onPreviewFrame is called for each frame at the camera preview.
+	// We'll convert each frame data (byte[] data) from YUV to RGB and apply 
 	// some effect like blur or gray scale
+	
 	@Override
 	public void onPreviewFrame(byte[] data, Camera camera) {
 
@@ -187,20 +184,19 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Ca
 			Camera.Parameters parameters = mCamera.getParameters();	    		
 			Size imageSize = parameters.getPreviewSize() ;
 
-			Bitmap rgbBitmap = convertYuvToRgb(data,imageSize);
+			//Bitmap rgbBitmap = RenderScriptHelper.convertYuvToRgb(mRs,data,imageSize);
 
+			Bitmap rgbBitmap = RenderScriptHelper.convertYuvToRgbIntrinsic(mRs,data,imageSize );
 
-			//Bitmap rgbBitmap = convertYuvToRgbIntrinsic(data,imageSize );
-
-			//Bitmap grayScaleBitmap = applyGrayScaleEffectIntrinsic(rgbBitmap, imageSize);
-
-			Bitmap blurBitmap = applyBlurEffectIntrinsic(rgbBitmap,imageSize );
+			Bitmap grayScaleBitmap = RenderScriptHelper.applyGrayScaleEffectIntrinsic(mRs,rgbBitmap, imageSize);
+												
+			//Bitmap blurBitmap = RenderScriptHelper.applyBlurEffectIntrinsic(mRs,rgbBitmap,imageSize );
 
 
 			// Create an ImageView and put the output bitmap after manipulation (blur or gray scale) into it
 			// and then display that imageView 
 			ImageView image = new ImageView(this);
-			image.setImageBitmap(blurBitmap) ;
+			image.setImageBitmap(grayScaleBitmap) ;
 			setContentView(image) ;
 
 			//new PreviewAsyncTask().execute(data);// didn't see improvements when running in Async task
@@ -252,127 +248,13 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Ca
 	}
 
 
-	private Bitmap applyGrayScaleEffectIntrinsic(Bitmap inBitmap, Size imageSize) {
 
-		Bitmap grayBitmap = inBitmap.copy(inBitmap.getConfig(), true);
-
-		Allocation aIn = Allocation.createFromBitmap(mRs, inBitmap,Allocation.MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
-		Allocation aOut = Allocation.createTyped(mRs, aIn.getType());
-
-		//Make the image grey scale
-		final ScriptIntrinsicColorMatrix scriptColor = ScriptIntrinsicColorMatrix.create(mRs, Element.U8_4(mRs));
-		scriptColor.setGreyscale();
-		scriptColor.forEach(aIn, aOut);
-		aOut.copyTo(grayBitmap);
-
-		return grayBitmap ;
-
-	}
-
-
-
-
-	private Bitmap applyBlurEffectIntrinsic(Bitmap inBitmap, Size imageSize) {
-
-		Bitmap outBitmap = inBitmap.copy(inBitmap.getConfig(), true);
-
-		Allocation aIn = Allocation.createFromBitmap(mRs, inBitmap,Allocation.MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
-		Allocation aOut = Allocation.createTyped(mRs, aIn.getType());
-
-		//Blur the image
-		ScriptIntrinsicBlur script = ScriptIntrinsicBlur.create(mRs, Element.U8_4(mRs));
-		script.setRadius(15f);
-		script.setInput(aIn);
-		script.forEach(aOut);
-		aOut.copyTo(outBitmap);
-
-		return outBitmap ;
-	}
-
-
-	// Convert to RGB using Intrinsic render script
-	private Bitmap convertYuvToRgbIntrinsic(byte[] data, Size imageSize) {
-
-		int imageWidth = imageSize.width ;
-		int imageHeight = imageSize.height ;
-
-		ScriptIntrinsicYuvToRGB yuvToRgbIntrinsic = ScriptIntrinsicYuvToRGB.create(mRs, Element.RGBA_8888(mRs));
-
-		// Create the input allocation
-		Type.Builder yuvType = new Type.Builder(mRs, Element.U8(mRs))
-		.setX(imageWidth)
-		.setY(imageHeight)
-		.setYuvFormat(android.graphics.ImageFormat.NV21);
-
-		Allocation aIn = Allocation.createTyped(mRs, yuvType.create(), Allocation.USAGE_SCRIPT);
-		// Set the YUV frame data into the input allocation
-		aIn.copyFrom(data);
-
-
-		// Create the output allocation
-		Type.Builder rgbType = new Type.Builder(mRs, Element.RGBA_8888(mRs))
-		.setX(imageWidth)
-		.setY(imageHeight);
-
-		Allocation aOut = Allocation.createTyped(mRs, rgbType.create(), Allocation.USAGE_SCRIPT);
-
-
-
-		yuvToRgbIntrinsic.setInput(aIn);
-		// Run the script for every pixel on the input allocation and put the result in aOut
-		yuvToRgbIntrinsic.forEach(aOut);
-
-		// Create an output bitmap and copy the result into that bitmap
-		Bitmap outBitmap = Bitmap.createBitmap(imageWidth, imageHeight, Bitmap.Config.ARGB_8888);
-		aOut.copyTo(outBitmap);
-
-		return outBitmap ;
-
-	}
-
-
-
-	// Convert to RGB using render script - with Yuv2Rgb.rs
-	protected Bitmap convertYuvToRgb(byte[] data, Size imageSize) {
-
-		int imageWidth = imageSize.width ;
-		int imageHeight = imageSize.height ;
-
-		// Input allocation
-		Type.Builder yuvType = new Type.Builder(mRs, Element.createPixel(mRs, Element.DataType.UNSIGNED_8, Element.DataKind.PIXEL_YUV))
-		.setX(imageWidth)
-		.setY(imageHeight)
-		.setMipmaps(false)
-		.setYuvFormat(ImageFormat.NV21);
-		Allocation ain = Allocation.createTyped(mRs, yuvType.create(), Allocation.USAGE_SCRIPT);
-		ain.copyFrom(data);
-
-
-		// output allocation
-		Type.Builder rgbType = new Type.Builder(mRs, Element.RGBA_8888(mRs))
-		.setX(imageWidth)
-		.setY(imageHeight)
-		.setMipmaps(false);
-
-		Allocation aOut = Allocation.createTyped(mRs, rgbType.create(), Allocation.USAGE_SCRIPT);
-
-
-		// Create the script 
-		ScriptC_Yuv2Rgb yuvScript = new ScriptC_Yuv2Rgb(mRs); 
-		// Bind to script level -  set the allocation input and parameters from the java into the script level (thru JNI)
-		yuvScript.set_gIn(ain);
-		yuvScript.set_width(imageWidth);
-		yuvScript.set_height(imageHeight);
-
-		// invoke the script conversion method
-		yuvScript.forEach_yuvToRgb(ain, aOut);
-
-		Bitmap outBitmap = Bitmap.createBitmap(imageWidth, imageHeight, Bitmap.Config.ARGB_8888);
-		aOut.copyTo(outBitmap) ;
-
-		return outBitmap ;
-
-	}
+	
+	
+	
+	
+	
+	
 
 
 	// Not using that for now
@@ -384,10 +266,10 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback, Ca
 			Camera.Parameters parameters = mCamera.getParameters();	    		
 			Size imageSize = parameters.getPreviewSize() ;
 
-			//Bitmap rgbBitmap = convertYuvToRgb(data,imageSize);
-			Bitmap rgbBitmap = convertYuvToRgbIntrinsic(data,imageSize );
-			Bitmap blurBitmap = applyBlurEffectIntrinsic(rgbBitmap,imageSize );
-			Bitmap grayScaleBitmap = applyGrayScaleEffectIntrinsic(blurBitmap, imageSize);
+			//Bitmap rgbBitmap = RenderScriptHelper.convertYuvToRgb(mRs,data,imageSize);
+			Bitmap rgbBitmap = RenderScriptHelper.convertYuvToRgbIntrinsic(mRs,data,imageSize );
+			Bitmap blurBitmap = RenderScriptHelper.applyBlurEffectIntrinsic(mRs,rgbBitmap,imageSize );
+			Bitmap grayScaleBitmap = RenderScriptHelper.applyGrayScaleEffectIntrinsic(mRs,blurBitmap, imageSize);
 
 
 			//canvas.drawBitmap(rgbBitmap, x, y, null) ;
---
+++ src/com/example/yuv2rgbrenderscript/RenderScriptHelper.java
@@ -0,0 +1,147 @@
+package com.example.yuv2rgbrenderscript;
+
+import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
+import android.hardware.Camera.Size;
+import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.Element;
+import android.support.v8.renderscript.RenderScript;
+import android.support.v8.renderscript.ScriptIntrinsicBlur;
+import android.support.v8.renderscript.ScriptIntrinsicColorMatrix;
+import android.support.v8.renderscript.ScriptIntrinsicYuvToRGB;
+import android.support.v8.renderscript.Type;
+
+final class RenderScriptHelper {
+
+
+
+
+
+
+	// Convert to RGB using Intrinsic render script
+	public static Bitmap convertYuvToRgbIntrinsic(RenderScript rs,byte[] data, Size imageSize) {
+
+		int imageWidth = imageSize.width ;
+		int imageHeight = imageSize.height ;
+
+		ScriptIntrinsicYuvToRGB yuvToRgbIntrinsic = ScriptIntrinsicYuvToRGB.create(rs, Element.RGBA_8888(rs));
+
+		// Create the input allocation
+		Type.Builder yuvType = new Type.Builder(rs, Element.U8(rs))
+		.setX(imageWidth)
+		.setY(imageHeight)
+		.setYuvFormat(android.graphics.ImageFormat.NV21);
+
+		Allocation aIn = Allocation.createTyped(rs, yuvType.create(), Allocation.USAGE_SCRIPT);
+		// Set the YUV frame data into the input allocation
+		aIn.copyFrom(data);
+
+
+		// Create the output allocation
+		Type.Builder rgbType = new Type.Builder(rs, Element.RGBA_8888(rs))
+		.setX(imageWidth)
+		.setY(imageHeight);
+
+		Allocation aOut = Allocation.createTyped(rs, rgbType.create(), Allocation.USAGE_SCRIPT);
+
+
+
+		yuvToRgbIntrinsic.setInput(aIn);
+		// Run the script for every pixel on the input allocation and put the result in aOut
+		yuvToRgbIntrinsic.forEach(aOut);
+
+		// Create an output bitmap and copy the result into that bitmap
+		Bitmap outBitmap = Bitmap.createBitmap(imageWidth, imageHeight, Bitmap.Config.ARGB_8888);
+		aOut.copyTo(outBitmap);
+
+		return outBitmap ;
+
+	}
+
+
+
+	// Convert to RGB using render script - with Yuv2Rgb.rs
+	public static Bitmap convertYuvToRgb(RenderScript rs,byte[] data, Size imageSize) {
+
+		int imageWidth = imageSize.width ;
+		int imageHeight = imageSize.height ;
+
+		// Input allocation
+		Type.Builder yuvType = new Type.Builder(rs, Element.createPixel(rs, Element.DataType.UNSIGNED_8, Element.DataKind.PIXEL_YUV))
+		.setX(imageWidth)
+		.setY(imageHeight)
+		.setMipmaps(false)
+		.setYuvFormat(ImageFormat.NV21);
+		Allocation ain = Allocation.createTyped(rs, yuvType.create(), Allocation.USAGE_SCRIPT);
+		ain.copyFrom(data);
+
+
+		// output allocation
+		Type.Builder rgbType = new Type.Builder(rs, Element.RGBA_8888(rs))
+		.setX(imageWidth)
+		.setY(imageHeight)
+		.setMipmaps(false);
+
+		Allocation aOut = Allocation.createTyped(rs, rgbType.create(), Allocation.USAGE_SCRIPT);
+
+
+		// Create the script 
+		ScriptC_Yuv2Rgb yuvScript = new ScriptC_Yuv2Rgb(rs); 
+		// Bind to script level -  set the allocation input and parameters from the java into the script level (thru JNI)
+		yuvScript.set_gIn(ain);
+		yuvScript.set_width(imageWidth);
+		yuvScript.set_height(imageHeight);
+
+		// invoke the script conversion method
+		yuvScript.forEach_yuvToRgb(ain, aOut);
+
+		Bitmap outBitmap = Bitmap.createBitmap(imageWidth, imageHeight, Bitmap.Config.ARGB_8888);
+		aOut.copyTo(outBitmap) ;
+
+		return outBitmap ;
+
+	}
+
+
+
+	
+
+
+	public static Bitmap applyBlurEffectIntrinsic(RenderScript rs,Bitmap inBitmap, Size imageSize) {
+
+		Bitmap outBitmap = inBitmap.copy(inBitmap.getConfig(), true);
+
+		Allocation aIn = Allocation.createFromBitmap(rs, inBitmap,Allocation.MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
+		Allocation aOut = Allocation.createTyped(rs, aIn.getType());
+
+		//Blur the image
+		ScriptIntrinsicBlur script = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+		script.setRadius(15f);
+		script.setInput(aIn);
+		script.forEach(aOut);
+		aOut.copyTo(outBitmap);
+
+		return outBitmap ;
+	}
+	
+	
+	
+	public static Bitmap applyGrayScaleEffectIntrinsic(RenderScript rs,Bitmap inBitmap, Size imageSize) {
+
+		Bitmap grayBitmap = inBitmap.copy(inBitmap.getConfig(), true);
+
+		Allocation aIn = Allocation.createFromBitmap(rs, inBitmap,Allocation.MipmapControl.MIPMAP_NONE, Allocation.USAGE_SCRIPT);
+		Allocation aOut = Allocation.createTyped(rs, aIn.getType());
+
+		//Make the image grey scale
+		final ScriptIntrinsicColorMatrix scriptColor = ScriptIntrinsicColorMatrix.create(rs, Element.U8_4(rs));
+		scriptColor.setGreyscale();
+		scriptColor.forEach(aIn, aOut);
+		aOut.copyTo(grayBitmap);
+
+		return grayBitmap ;
+
+	}
+
+
+}
